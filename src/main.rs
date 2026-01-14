ef826e6b2cfb5128286d540094cfb620c1bea4e6
ayush <ayush.mehta010906@gmail.com>
2026-01-14 13:36:40 +0000

    directing errors to std errors

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguements: {err}");
+        eprintln!("Problem parsing arguements: {err}");
         process::exit(1);
     });
 
     if let Err(e) = run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
